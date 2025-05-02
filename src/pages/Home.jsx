e0045def7e2cdefd94dbf27f8f44059de713610f
aaronqintanilla <aquinta2006@gmail.com>
2025-05-03 01:58:04 +0200

    addNav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/App.css';
+import bonsai from '../assets/bonsai.png';
 
 function Home() {
   return (
     <div>
+      <nav class="bg-white border-gray-200 dark:bg-gray-900 px-10">
+        <div class="w-full flex flex-wrap items-center justify-between p-4">
+          <Link to="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={bonsai} class="h-20" alt="Bonsai Logo" />
+            <span class="self-center text-4xl font-semibold whitespace-nowrap dark:text-white">Plantinance</span>
+          </Link>
+          <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+          <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul class="text-2xl font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-14 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+              </li>
+              <li>
+                <Link to="dashboard" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="login" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Log in</Link>
+              </li>
+              <li>
+                <Link to="register" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign up</Link>
+              </li>
+              <li>
+                <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <main>
-        <h1 className="text-[5rem] font-bold">Hello world</h1>
+        <p className="text-red-400 text-center text-[5rem] font-bold">Plantinance</p>
       </main>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/dashboard">Dashboard</Link>
     </div>
   );
 }
