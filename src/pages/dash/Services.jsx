e339b53aa4223bcbf2d10ca1f23860812ca8787f
Max Vilà Ruiz <max16506@gmail.com>
2025-05-04 00:50:29 +0200

    Add localStorage config to Dash/Services

--- src/pages/dash/Services.jsx
+++ src/pages/dash/Services.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import PieDiagram from '../../components/diagram';
 import '../../styles/App.css';
+import { lightViewPosition } from 'three/src/nodes/TSL.js';
 
 function Services() {
   const navigate = useNavigate();
@@ -11,27 +12,57 @@ function Services() {
     navigate('/login');
   };
 
-  useEffect (() => {
-    const income_ = localStorage.getItem('income');
-    const rent1_ = localStorage.getItem('rent1');
-    const rent2_ = localStorage.getItem('rent2');
-    const household1_ = localStorage.getItem('household1');
-    const household2_ = localStorage.getItem('household2');
-    const living1_ = localStorage.getItem('living1');
-    const living2_ = localStorage.getItem('living2');
-    const extras1_ = localStorage.getItem('extras1');
-    const extras2_ = localStorage.getItem('extras2');
-
-    document.getElementById('income').innerText = income_;
-    document.getElementById('rent1').innerText = rent1_;
-    document.getElementById('rent2').innerText = rent2_;
-    document.getElementById('household1').innerText = household1_;
-    document.getElementById('household2').innerText = household2_;
-    document.getElementById('living1').innerText = living1_;
-    document.getElementById('living2').innerText = living2_;
-    document.getElementById('extras1').innerText = extras1_;
-    document.getElementById('extras2').innerText = extras2_;
-  });
+  useEffect(() => {
+    const income = Number(localStorage.getItem('income') || 0);
+    const rent = Number(localStorage.getItem('rent') || 0);
+    const household = Number(localStorage.getItem('household') || 0);
+    const living = Number(localStorage.getItem('living') || 0);
+    const extras = Number(localStorage.getItem('extras') || 0);
+    const saved = Number(localStorage.getItem('saved') || 0);
+  
+    const rent2 = (rent / income) * 100;
+    const household2 = (household / income) * 100;
+    const living2 = (living / income) * 100;
+    const extras2 = (extras / income) * 100;
+    const saved2 = (saved / income) * 100;
+  
+    const rent3 = Number(document.getElementById('rent3')?.value || 0);
+    if (rent3 < rent2) document.getElementById('rent4').style.background = 'red';
+    else if (rent3 === rent2) document.getElementById('rent4').style.background = 'orange';
+    else document.getElementById('rent4').style.background = 'green';
+  
+    const household3 = Number(document.getElementById('household3')?.value || 0);
+    if (household3 < household2) document.getElementById('household4').style.background = 'red';
+    else if (household3 === household2) document.getElementById('household4').style.background = 'orange';
+    else document.getElementById('household4').style.background = 'green';
+  
+    const living3 = Number(document.getElementById('living3')?.value || 0);
+    if (living3 < living2) document.getElementById('living4').style.background = 'red';
+    else if (living3 === living2) document.getElementById('living4').style.background = 'orange';
+    else document.getElementById('living4').style.background = 'green';
+  
+    const extras3 = Number(document.getElementById('extras3')?.value || 0);
+    if (extras3 < extras2) document.getElementById('extras4').style.background = 'red';
+    else if (extras3 === extras2) document.getElementById('extras4').style.background = 'orange';
+    else document.getElementById('extras4').style.background = 'green';
+  
+    const saved3 = Number(document.getElementById('saved3')?.value || 0);
+    if (saved3 < saved2) document.getElementById('saved4').style.background = 'green';
+    else if (saved3 === saved2) document.getElementById('saved4').style.background = 'orange';
+    else document.getElementById('saved4').style.background = 'red';
+  
+    document.getElementById('income').innerText = income;
+    document.getElementById('rent1').innerText = rent;
+    document.getElementById('rent2').innerText = rent2.toFixed(0);
+    document.getElementById('household1').innerText = household;
+    document.getElementById('household2').innerText = household2.toFixed(0);
+    document.getElementById('living1').innerText = living;
+    document.getElementById('living2').innerText = living2.toFixed(0);
+    document.getElementById('extras1').innerText = extras;
+    document.getElementById('extras2').innerText = extras2.toFixed(0);
+    document.getElementById('saved1').innerText = saved;
+    document.getElementById('saved2').innerText = saved2.toFixed(0);
+  }, []);
 
   return (
     <div className="flex w-full h-auto justify-center pb-20">
@@ -139,7 +170,8 @@ function Services() {
                 </td>
                 <td id='rent3' class="px-6 py-4">
                 </td>
-                <td id='rent4' class="px-6 py-4">
+                <td class="px-6 py-4">
+                  <div id='rent4' style="width: 30px; height: 30px; border-radius: 50%; background-color: gray;"></div>
                 </td>
               </tr>
               <tr class="text-center border-b border-gray-700 bg-gray-800 hover:bg-gray-600">
@@ -152,7 +184,8 @@ function Services() {
                 </td>
                 <td id='household3' class="px-6 py-4">
                 </td>
-                <td id='household4' class="px-6 py-4">
+                <td class="px-6 py-4">
+                  <div id='household4' style="width: 30px; height: 30px; border-radius: 50%; background-color: gray;"></div>
                 </td>
               </tr>
               <tr class="text-center border-b border-gray-700 bg-gray-800 hover:bg-gray-600">
@@ -165,7 +198,8 @@ function Services() {
                 </td>
                 <td id='living3' class="px-6 py-4">
                 </td>
-                <td id='living4' class="px-6 py-4">
+                <td class="px-6 py-4">
+                  <div id='living4' style="width: 30px; height: 30px; border-radius: 50%; background-color: gray;"></div>
                 </td>
               </tr>
               <tr class="text-center border-b border-gray-700 bg-gray-800 hover:bg-gray-600">
@@ -178,7 +212,22 @@ function Services() {
                 </td>
                 <td id='extras3' class="px-6 py-4">
                 </td>
-                <td id='extras4' class="px-6 py-4">
+                <td class="px-6 py-4">
+                  <div id='extras4' style="width: 30px; height: 30px; border-radius: 50%; background-color: gray;"></div>
+                </td>
+              </tr>
+              <tr class="text-center border-b border-gray-700 bg-gray-800 hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">
+                  Saved
+                </th>
+                <td id='saved1' class="px-6 py-4">
+                </td>
+                <td id='saved2' class="px-6 py-4">
+                </td>
+                <td id='saved3' class="px-6 py-4">
+                </td>
+                <td class="px-6 py-4">
+                  <div id='saved4' style="width: 30px; height: 30px; border-radius: 50%; background-color: gray;"></div>
                 </td>
               </tr>
             </tbody>
