1c071633f367fe0a8362d01eb6791d52ba706d6f
Max Vilà Ruiz <max16506@gmail.com>
2025-05-04 00:55:32 +0200

    Fix PieDiagram component

--- src/components/diagram_.jsx
+++ src/components/diagram_.jsx
@@ -1,26 +1,26 @@
 import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF0000'];
 
 export default function PieDiagram() {
-    const data = [];
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const saves = localStorage.getItem('saved');
-        const rent = localStorage.getItem('rent');
-        const household = localStorage.getItem('household');
-        const living = localStorage.getItem('living');
-        const primary = household + living;
-        const extras = localStorage.getItem('extras');
+  useEffect(() => {
+    const saves = Number(localStorage.getItem('saved') || 0);
+    const rent = Number(localStorage.getItem('rent') || 0);
+    const household = Number(localStorage.getItem('household') || 0);
+    const living = Number(localStorage.getItem('living') || 0);
+    const extras = Number(localStorage.getItem('extras') || 0);
+    const primary = household + living;
 
-        const data = [
-            { name: 'Saves', value: saves},
-            { name: 'Rent', value: rent },
-            { name: 'Primary expenses', value: primary },
-            { name: 'Leisure expenses', value: extras },
-        ];
-    });
+    setData([
+      { name: 'Saves', value: saves },
+      { name: 'Rent', value: rent },
+      { name: 'Primary expenses', value: primary },
+      { name: 'Leisure expenses', value: extras },
+    ]);
+  }, []);
 
   return (
     <PieChart width={400} height={400}>
